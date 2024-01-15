42d7d50aca40f032886d5f79a76af310e6c625c0
mryeminthu <dhammaarchive@hotmail.com>
2024-01-15 20:02:52 +0630

    Add associations to Expense model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_15_131931) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_15_133025) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_15_131931) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "expenses", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_expenses_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +46,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_15_131931) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "expenses", "categories"
 end
