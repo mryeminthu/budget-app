14e8636e2781e90988981ab218d991d83a2c776f
mryeminthu <dhammaarchive@hotmail.com>
2024-01-15 20:39:04 +0630

    Fix linter

--- spec/views/expenses/show.html.erb_spec.rb
+++ spec/views/expenses/show.html.erb_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "expenses/show", type: :view do
+RSpec.describe 'expenses/show', type: :view do
   before(:each) do
     assign(:expense, Expense.create!(
-      name: "Name",
-      amount: "9.99",
-      category: nil
-    ))
+                       name: 'Name',
+                       amount: '9.99',
+                       category: nil
+                     ))
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
     expect(rendered).to match(/Name/)
     expect(rendered).to match(/9.99/)
