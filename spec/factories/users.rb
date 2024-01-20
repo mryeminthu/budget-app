ba0c80ced91b9ef246f616a993fd464420700791
mryeminthu <dhammaarchive@hotmail.com>
2024-01-20 19:15:33 +0630

    Add 2 Capybara test

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+    factory :user do
+      name { 'John Doe' }
+      email { 'john@example.com' }
+      password { 'password' }
+      password_confirmation { 'password' }
+    end
+  end
+  
